f7498947050c1539bd5a9aafc212b91d5d4940a0
matheusgdp <matheusdepaul@hotmail.com>
2019-10-03 23:43:41 -0300

    Create todo form

---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Grid from '../template/grid'
+
+export default props => (
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input type="text" id="decription" className="form-control" placeholder="Adicione uma tarefa"/>
+        </Grid>
+        <Grid cols='12 3 2'>
+            <button className="btn btn-primary">
+                <i className="fa fa-plus"></i>
+            </button>
+        </Grid>
+    </div>
+)
