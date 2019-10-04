b99e1bb5fd33340bd555c258a0b14baffb1bac83
matheusgdp <matheusdepaul@hotmail.com>
2019-10-04 18:30:13 -0300

    Import custom.css

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,5 +1,7 @@
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/font-awesome/css/font-awesome.min.css'
+import '../template/custom.css'
+
 import { HashRouter } from 'react-router-dom'
 
 import React from 'react'
